e419e237c7738f060b31e126922c45f136ccead1
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-12-29 08:45:34 +0400

    feat:mvc layout

---
+++ DataTransfer/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataTransfer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var view = new ViewResult();
+            view.ViewName = "Index";
+
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult GetFile()
+        {
+            return File("~/img/image.png", "image/png");
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+    }
+}
